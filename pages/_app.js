6430d0cc94a5ce23934b8accf879adffbea3e1d3
AsymmetricFrequency <93012550+AsymmetricFrequency@users.noreply.github.com>
2024-06-26 21:24:29 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -22,7 +22,7 @@ export default function MyApp(props) {
     <Fragment>
       <Head>
         <title>Wazaaa</title>
-        <meta>
+        <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
